498694caf45afbaf134da5c9e0caab8f5cddcd43
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2023-03-11 18:16:52 +0000

    Build: Fix error

--- copy-types.js
+++ copy-types.js
@@ -1,9 +1,11 @@
-const { readdirSync, readFileSync, writeFileSync } = require("fs");
+const { existsSync, mkdirSync, readdirSync, readFileSync, writeFileSync } = require("fs");
 const runFormat = (string) =>
     require("prettier").format(string, Object.assign({ parser: "typescript" }, require("./.prettierrc")));
 const regex = /"\.\.\/src\//g;
 const ReadBasePath = "./types";
 const WriteBasePath = "./dist/types";
+
+if (!existsSync(WriteBasePath)) mkdirSync(WriteBasePath, { recursive: true });
 
 for (const FileName of readdirSync(ReadBasePath)) {
     const ReadFilePath = ReadBasePath + "/" + FileName;
