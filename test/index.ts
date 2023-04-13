9bcd40e1999222d6dd59e322e0863fd175edd767
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2023-04-13 16:05:24 +0000

    Test: Fix error

--- test/index.ts
+++ test/index.ts
@@ -6,10 +6,11 @@ import * as path from "node:path";
 import { Client } from "@dist/cjs/index";
 import Mocha from "mocha";
 
-if (!process.env.TEST_BOT_PASS) throw new Error("Test account's password was not provided.");
+if (!process.env.TEST_BOT_PASS || !process.env.TEST_BOT_NAME)
+    throw new Error("Test account's username or password was not provided.");
 
 const PSClient = new Client({
-    name: "Dirain's Bot",
+    name: process.env.TEST_BOT_NAME,
     pass: process.env.TEST_BOT_PASS,
     autoJoin: ["botdevelopment"],
 });
