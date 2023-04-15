b97471e92d620495753c47790a7f84f83bd48c0c
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2023-04-15 15:57:31 +0000

    Test: Fix more grammar on message test

--- test/modules/Client.ts
+++ test/modules/Client.ts
@@ -39,7 +39,7 @@ describe("Client", function () {
         });
     });
     describe("Message", function () {
-        it("Should send a message in PM", function (done) {
+        it("Should send a message to a User", function (done) {
             this.timeout(5 * 1000);
             this.slow(300);
             client.once("messageCreate", (message) => {
@@ -52,7 +52,7 @@ describe("Client", function () {
             });
             client.user?.send(testMessageContent);
         });
-        it("Should send a message in Room", function (done): void {
+        it("Should send a message to a Room", function (done): void {
             if (process.env["CI"]) return this.skip();
             this.timeout(5 * 1000);
             this.slow(300);
