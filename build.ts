af63f884855aea4d7e5d03b98a28badcfcf8705b
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2023-08-06 18:49:20 +0000

    Build: Delete built code before running esbuild

--- build.ts
+++ build.ts
@@ -1,5 +1,6 @@
 "use strict";
 
+import * as fs from "node:fs";
 import * as path from "node:path";
 
 import { build, buildSync } from "esbuild";
@@ -7,6 +8,7 @@ import { aliasPath } from "esbuild-plugin-alias-path";
 import { globSync } from "glob";
 import { cloneDeep } from "lodash";
 
+const outputDir = path.resolve(__dirname, "dist");
 const targetFiles = globSync(["src/**/*.ts", "data/**/*.ts", "config/**/*.ts"]);
 const targetTestFiles = globSync(["test/**/*.ts"]);
 
@@ -20,13 +22,15 @@ const config = {
     write: true,
 };
 
+fs.rmSync(outputDir, { recursive: true });
+
 console.log("Transpiling to CommonJS...");
 
 // @ts-expect-error format should be assignable
 // prettier-ignore
 buildSync(Object.assign(cloneDeep(config), {
     format: "cjs",
-    outdir: "dist/cjs",
+    outdir: path.join(outputDir, "cjs"),
     tsconfig: path.resolve(__dirname, "tsconfig.cjs.json"),
 }));
 
@@ -37,7 +41,7 @@ console.log("Transpiling to ES Module...");
 buildSync(Object.assign(cloneDeep(config), {
     format: "esm",
     outExtension: { ".js": ".mjs" },
-    outdir: "dist/esm",
+    outdir: path.join(outputDir, "esm"),
     tsconfig: path.resolve(__dirname, "tsconfig.esm.json"),
 }));
 
